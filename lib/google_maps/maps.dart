c373c25d63f485a4b2bab98fb8bd4a5b462a3399
MohamedAatef12 <mohamedateefmmeekk@gmail.com>
2024-02-18 01:15:58 +0200

    add google map

---
+++ lib/google_maps/maps.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Maps extends StatelessWidget {
+  const Maps({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(20, 45),
+          zoom: 4,
+        ),
+      ),
+    );
+  }
+}
