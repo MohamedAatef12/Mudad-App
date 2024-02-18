cc22a4bc46e611e71f8e6a80c26a682f909345bb
MohamedAatef12 <mohamedateefmmeekk@gmail.com>
2024-02-18 09:47:03 +0200

    add google map

--- lib/google_maps/maps.dart
+++ lib/google_maps/maps.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class Maps extends StatelessWidget {
-  const Maps({
-    super.key,
-  });
+class MosquesMap extends StatefulWidget {
+  const MosquesMap({Key? key}) : super(key: key);
+
+  @override
+  State<MosquesMap> createState() => _MapsState();
+}
+
+class _MapsState extends State<MosquesMap> {
+  late GoogleMapController controllerMapController;
+  final Set<Marker> markers = {};
+  late CameraPosition initialCameraPosition;
+
+  @override
+  void initState() {
+    initialCameraPosition = const CameraPosition(
+      target: LatLng(23.8859, 45.0792), // Center of Saudi Arabia
+      zoom: 6,
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(20, 45),
-          zoom: 4,
-        ),
+        onMapCreated: (controller) {
+          controllerMapController = controller;
+        },
+        initialCameraPosition: initialCameraPosition,
       ),
     );
   }
