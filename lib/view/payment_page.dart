44debbd32d815eaaa71bd2378300ee47db87522f
AhmedMostafa809 <am2098432@gmail.com>
2024-03-20 01:09:30 +0200

    payment page updates and username saved in storage

--- lib/view/payment_page.dart
+++ lib/view/payment_page.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:mudad_app/google_maps/mosque_map/build_product.dart';
 import 'package:mudad_app/reusable_widgets/custom_paymment_card.dart';
+import 'package:mudad_app/reusable_widgets/text_field.dart';
 import 'package:mudad_app/services/payment_service/payment_controller.dart';
-import 'package:mudad_app/view_model/orders_cubit/orders_cubit.dart';
-
 import '../app_constants/app_colors.dart';
 import '../app_constants/app_text_styles.dart';
 
+TextEditingController MudadCodeController = TextEditingController();
+
 class PaymentPage extends StatelessWidget {
-  // final String selectedImage;
-  const PaymentPage({ Key? key}) : super(key: key);
+  const PaymentPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
-        physics: const BouncingScrollPhysics(),
+        // physics: const BouncingScrollPhysics(),
         children: [
           Image.asset(
             "assets/payment_assets/upper side.png",
@@ -46,36 +48,36 @@ class PaymentPage extends StatelessWidget {
                   )),
             ),
           ),
-        Container(
-          height: 300,
-          width: double.infinity,
-          child: Column(children: [Text('Location'),Text(totalOrder.toString()),
-          Row(
-            children: [
-              Text(products.first),
-              Image.network(productImages.first,height: 70,),
-              Text(orderCounter.first.toString()),
-
-              Text(productPrice.first.toString()+"X"+orderCounter.first.toString()+"=" +p1Total.toString())
-            ],
-          ),
-
-
-            Row(
-            children: [
-              Text(products.last),
-              Image.network(productImages.last,height: 70,),
-              Text(orderCounter.last.toString()),
-              
-              Text(productPrice.last.toString()+"X"+orderCounter.last.toString()+"=" +p2Total.toString())
-            ],
-          )
-          ],),
-        ),
+          // Container(
+          //   height: 300,
+          //   width: double.infinity,
+          //   child: Column(children: [Text('Location'),Text(totalOrder.toString()),
+          //   Row(
+          //     children: [
+          //       Text(products.first),
+          //       Image.network(productImages.first,height: 70,),
+          //       Text(orderCounter.first.toString()),
+          //
+          //       Text(productPrice.first.toString()+"X"+orderCounter.first.toString()+"=" +p1Total.toString())
+          //     ],
+          //   ),
+          //
+          //
+          //     Row(
+          //     children: [
+          //       Text(products.last),
+          //       Image.network(productImages.last,height: 70,),
+          //       Text(orderCounter.last.toString()),
+          //
+          //       Text(productPrice.last.toString()+"X"+orderCounter.last.toString()+"=" +p2Total.toString())
+          //     ],
+          //   )
+          //   ],),
+          // ),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-             const Icon(Icons.lock),
+              const Icon(Icons.lock),
               const SizedBox(
                 width: 2,
               ),
@@ -188,118 +190,227 @@ class PaymentPage extends StatelessWidget {
           //     ],
           //   ),
           // ),
-          // PaymentCard(
-          //   width: 150,
-          //   height: 80,
-          //   onTap: () {},
-          //   child: Row(
-          //     crossAxisAlignment: CrossAxisAlignment.center,
-          //     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //     children: [
-          //       Image.asset(
-          //         "assets/payment_assets/voucher-coupon-svgrepo-com 1.png",
-          //         fit: BoxFit.fitHeight,
-          //       ),
-          //       const Text(
-          //         "بطاقات الخصم",
-          //         style: TextStyle(
-          //           fontSize: 30,
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          // Container(
-          //   height: MediaQuery.of(context).size.height * .3,
-          //   color: AppColors.buttonColor,
-          //   child: Column(
-          //       mainAxisAlignment: MainAxisAlignment.spaceAround,
-          //       children: [
-          //         Container(
-          //           height: 40,
-          //           width: 180,
-          //           decoration: BoxDecoration(
-          //             color: Colors.white,
-          //             borderRadius: BorderRadius.circular(10),
-          //           ),
-          //           child: Center(
-          //               child: Text(
-          //             "مراجعه طلبك",
-          //             style:GoogleFonts.lalezar(
-          //                 fontSize: 20,
-          //                 fontWeight: FontWeight.w100,
-          //                 color: Colors.grey)
-          //           )),
-          //         ),
-          //         Row(
-          //           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          //           children: [
-          //             Container(
-          //               height: 130,
-          //               width: 130,
-          //               decoration: BoxDecoration(
-          //                 color: Colors.white,
-          //                 borderRadius: BorderRadius.circular(10),
-          //               ),
-          //               child: Column(
-          //                 mainAxisAlignment: MainAxisAlignment.center,
-          //                 children: [
-          //                   Text("عدد",
-          //                       style: GoogleFonts.lalezar(
-          //                           fontSize: 20,
-          //                           fontWeight: FontWeight.w100,
-          //                           color: Colors.grey)),
-          //                   const SizedBox(
-          //                     height: 10,
-          //                   ),
-          //                   Text("10 كرتونه ",
-          //                       style: GoogleFonts.lalezar(
-          //                           fontSize: 20,
-          //                           fontWeight: FontWeight.w100,
-          //                           color: Colors.black)),
-          //                 ],
-          //               ),
-          //             ),
-          //             SizedBox(
-          //               height: MediaQuery.of(context).size.height * .15,
-          //               child: const VerticalDivider(
-          //                 color: Colors.white,
-          //                 thickness: .6,
-          //                 width: 0,
-          //               ),
-          //             ),
-          //             Container(
-          //               height: 130,
-          //               width: 130,
-          //               decoration: BoxDecoration(
-          //                 color: Colors.white,
-          //                 borderRadius: BorderRadius.circular(10),
-          //               ),
-          //               child: FittedBox(
-          //                 child: Column(
-          //                   children: [
-          //                     Padding(
-          //                       padding: const EdgeInsets.all(20.0),
-          //                       child: Image.asset(
-          //                         "assets/payment_assets/bottles.png",
-          //                       ),
-          //                     ),
-          //                     Text(
-          //                       "عبوه330مx20 ",
-          //                       style: GoogleFonts.lalezar(
-          //                           fontSize: 40,
-          //                           fontWeight: FontWeight.w100,
-          //                           color: Colors.black),
-          //                     ),
-          //                   ],
-          //                 ),
-          //               ),
-          //             ),
-          //           ],
-          //         )
-          //       ],
-          //   ),
-          // ),
+          PaymentCard(
+            width: 150,
+            height: 80,
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                        title: Text("Enter_Mudad_code".tr),
+                        content: DefaultFormField(
+                            controller: MudadCodeController,
+                            keyboardType: TextInputType.text,
+                            obSecured: false));
+                  });
+            },
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  "assets/payment_assets/voucher-coupon-svgrepo-com 1.png",
+                  fit: BoxFit.fitHeight,
+                ),
+                Text(
+                  "vouchers".tr,
+                  style: const TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height * .3,
+            color: AppColors.buttonColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: 40,
+                  width: 180,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                      child: Text("مراجعه طلبك",
+                          style: GoogleFonts.lalezar(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w100,
+                              color: Colors.grey))),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * .175,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    shrinkWrap: true,
+                    // mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Container(
+                        height: 130,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("عدد",
+                                style: GoogleFonts.lalezar(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w100,
+                                    color: Colors.grey)),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text("${orderCounter.first.toString()} كرتونه ",
+                                style: GoogleFonts.lalezar(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w100,
+                                    color: Colors.black)),
+                            Text("${productPrice.first}X${orderCounter.first}=$p1Total")
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * .15,
+                        child: const VerticalDivider(
+                          color: Colors.white,
+                          thickness: .6,
+                          width: 0,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Container(
+                        height: 130,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: FittedBox(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Image.asset(
+                                  "assets/payment_assets/bottles.png",
+                                ),
+                              ),
+                              Text(
+                                "عبوه330مx20 ",
+                                style: GoogleFonts.lalezar(
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.w100,
+                                    color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 30,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * .15,
+                        child: const VerticalDivider(
+                          color: Colors.white,
+                          thickness: .6,
+                          width: 0,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 30,
+                      ),
+                      Container(
+                        height: 130,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("عدد",
+                                style: GoogleFonts.lalezar(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w100,
+                                    color: Colors.grey)),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Text("${orderCounter.last.toString()} ثلاجه مياه ",
+                                style: GoogleFonts.lalezar(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w100,
+                                    color: Colors.black)),
+                            Text("${productPrice.last}X${orderCounter.last}=$p2Total")
+                          ],
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height * .15,
+                        child: const VerticalDivider(
+                          color: Colors.white,
+                          thickness: .6,
+                          width: 0,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Container(
+                        height: 130,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: FittedBox(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: Image.network(
+                                  productImages.last.toString(),
+                                ),
+                              ),
+                              Text(
+                                "ثلاجه مياه ",
+                                style: GoogleFonts.lalezar(
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w100,
+                                    color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
